a7130d12ecdbc03502217ccfb7c303c2b2860ea9
CaptainRong <cr1092014130@gmail.com>
2023-12-13 13:33:09 +0800

    增加选定时间后显示该时间段内值班人员的内容，remove something useless

--- index/models.py
+++ index/models.py
@@ -35,8 +35,6 @@ class Users(models.Model):
         verbose_name = "成员管理"
 
 
-
-
 class Users_dai_ban(models.Model):
     id = models.AutoField(verbose_name="id", primary_key=True)
     name = models.CharField(verbose_name="姓名", max_length=33, default='', null=True)
@@ -165,6 +163,51 @@ class Matching(models.Model):
 
 
 
+
+class MyUsers(models.Model):
+    id = models.AutoField(verbose_name="id", primary_key=True)
+    user = models.CharField(verbose_name="姓名", max_length=33, default='', null=True)
+    Gender = [
+        (0, '男'),
+        (1, '女'),
+    ]
+    gender = models.SmallIntegerField(default=0, verbose_name='性别', choices=Gender)
+    department = models.ForeignKey(verbose_name="科室", to="Department", on_delete=models.CASCADE, null=True)
+    Active = [
+        (1, "正常"),
+        (2, "请假"),
+        (3, "出差"),
+        (4, "禁用"),
+    ]
+    is_active = models.SmallIntegerField(verbose_name="状态", default=1, choices=Active)
+    # 属于第几周
+    week = [
+        (0, '星期一'),
+        (1, '星期二'),
+        (2, '星期三'),
+        (3, '星期四'),
+        (4, '星期五'),
+        (5, '星期六'),
+        (6, '星期日')
+    ]
+    work_week = models.SmallIntegerField(default=0, verbose_name='周数', choices=week)
+    # 值班时间：早上，下午，晚上
+    work_time_choice = [
+        (0, '早上'),
+        (1, '下午'),
+        (2, '晚上')
+    ]
+    work_time = models.SmallIntegerField(default=0, verbose_name='早上', choices=work_time_choice)
+    # 单/双周 "Odd Week" "Even Week"。
+    oe_week_choice = [
+        (0, '双周'),
+        (1, '单周')
+    ]
+    oe_week = models.SmallIntegerField(default=0, verbose_name='单双周', choices=oe_week_choice)
+
+    def __str__(self):
+        return self.user
+
 # class Post(models.Model):
 #     id = models.AutoField(verbose_name="id", primary_key=True)
 #     name = models.CharField(verbose_name="职务", max_length=33, default='', null=True)
@@ -185,4 +228,3 @@ class Matching(models.Model):
 #         # 后台管理名
 #         verbose_name_plural = '职务管理'
 #         verbose_name = "职务管理"
-
--- index/views.py
+++ index/views.py
@@ -1,4 +1,6 @@
 import os
+# from plistlib import Data
+
 import openpyxl
 from django.http import StreamingHttpResponse, FileResponse, Http404
 from django.shortcuts import render, HttpResponse
@@ -6,11 +8,32 @@ from time import strftime
 import datetime
 import numpy as np
 from django.utils.encoding import escape_uri_path
-from django.db.models import Avg, Max, Min, F
+from django.db.models import Avg, Max, Min, F, Q
 from index.models import *
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+def get_week_type(date_str):
+    """获取单双周"""
+    date_object = datetime.datetime.strptime(date_str, "%Y-%m-%d")
+
+    # 获取ISO周数
+    iso_week_number = date_object.isocalendar()[1]
+
+    # 判断单周还是双周
+    week_type = 1 if iso_week_number % 2 == 1 else 0
+
+    return week_type
+
+
+def get_name(data):
+    """获取当日下姓名"""
+    week = data.weekday()
+    oe_week = get_week_type(data.strftime('%Y-%m-%d'))
+    name = MyUsers.objects.filter(Q(work_week=week) & Q(oe_week=oe_week))
+    return name
 
 
 # Create your views here.
@@ -19,131 +42,50 @@ def index(request):
     :param request:
     :return: date,users
     洗牌：
-        >>>np.random.permutation(10)
+        # >>>np.random.permutation(10)
     """
+    data_u = MyUsers.objects.all()
+    print(data_u)
     if request.method == 'GET':
+        context = {
+            'method': 'GET',
+            'data': data_u
+        }
+        return render(request, 'index/index.html', context)
+
+    date_start_f = request.POST['date_start']
+    date_end_f = request.POST['date_end']
+    date_start = datetime.datetime.strptime(date_start_f, "%Y-%m-%d")
+    date_end = datetime.datetime.strptime(date_end_f, "%Y-%m-%d")
+    delta = (date_end - date_start).days
+
+    if delta <= 0:
+        msg = "日期距离不能少于1天！"
         return render(request, 'index/index.html', locals())
-    elif request.method == "POST":
-        date_start_f = request.POST['date_start']
-        date_end_f = request.POST['date_end']
-        date_start = datetime.datetime.strptime(date_start_f, "%Y-%m-%d")
-        date_end = datetime.datetime.strptime(date_end_f, "%Y-%m-%d")
-        delta = (date_end - date_start).days
-        if delta <= 0:
-            msg = "日期距离不能少于1天！"
-            return render(request, 'index/index.html', locals())
-        elif delta >= 32:
-            msg = "日期距离不能大于31天！"
-            return render(request, 'index/index.html', locals())
-        # 白晚班列表
-        bai_ban_list = []
-        wan_ban_list = []
-        list_he = []
-        # 存在匹配对象进行添加白班列
-        not_pi = []
-        in_pi = []
-        bai_ban = Users.objects.filter(gender=1, is_active=1).order_by('counter')
-        for i in bai_ban:
-
-            try:
-                bai_ban_dai = Matching.objects.get(user__id=i.id).user_dai
-                new_dict = {"ban_yuan": {"id": i.id, "name": i.name},
-                            "dai_ban": {"id": bai_ban_dai.id, "name": bai_ban_dai.name}}
-                in_pi.append(bai_ban_dai.id)
-                bai_ban_list.append(new_dict)
-            except Matching.DoesNotExist:
-                not_pi.append(i.id)
-        # 没有白班对象处理
-        not_pi_user_list = Users.objects.in_bulk(id_list=not_pi).values()
-        user_dai_list = Users_dai_ban.objects.filter(gender=1, is_active=1).order_by('counter')
-        not_pi_user_dai_list = []
-        #
-        user_dai_min_counter = Users_dai_ban.objects.filter(is_active=1, gender=1).order_by('counter')
-        user_min_counter = Users.objects.filter(is_active=1, gender=1).order_by('counter')
-        for i in user_dai_list:
-            if i not in in_pi:
-                not_pi_user_dai_list.append(i)
-        # 查看带班和班员的数量
-
-        if len(not_pi_user_dai_list) < len(not_pi_user_list):
-            for index, item in enumerate(not_pi_user_dai_list):
-                new_dict = {'ban_yuan': {"id": user_min_counter[index].id, "name": user_min_counter[index].name},
-                            'dai_ban': {"id": item.id, "name": item.name}}
-                bai_ban_list.append(new_dict)
-        else:
-
-            for index, item in enumerate(not_pi_user_list):
-                new_dict = {'ban_yuan': {"id": item.id, "name": item.name},
-                            'dai_ban': {"id": user_dai_min_counter[index].id, "name": user_dai_min_counter[index].name}}
-                bai_ban_list.append(new_dict)
-
-        # 晚班处理
-        # 存在匹配对象进行添加白班列
-        not_pi = []
-        in_pi = []
-        bai_ban = Users.objects.filter(gender=0, is_active=1).order_by('counter')
-        for i in bai_ban:
-
-            try:
-                bai_ban_dai = Matching.objects.get(user_id=i.id)
-
-                new_dict = {"ban_yuan": {"id": i.id, "name": i.name},
-                            "dai_ban": {"id": bai_ban_dai.id, "name": bai_ban_dai.name}}
-                in_pi.append(bai_ban_dai.id)
-                wan_ban_list.append(new_dict)
-            except:
-                not_pi.append(i.id)
-
-        # 没有白班对象处理
-        not_pi_user_list = Users.objects.in_bulk(id_list=not_pi).values()
-        user_dai_list = Users_dai_ban.objects.filter(gender=0, is_active=1).order_by('counter')
-        not_pi_user_dai_list = []
-        #
-        user_dai_min_counter = Users_dai_ban.objects.filter(is_active=1, gender=0).order_by('counter')
-        user_min_counter = Users.objects.filter(is_active=1, gender=0).order_by('counter')
-        for i in user_dai_list:
-            if i not in in_pi:
-                not_pi_user_dai_list.append(i)
-        # 查看带班和班员的数量
-
-        if len(not_pi_user_dai_list) < len(not_pi_user_list):
-
-            for index, item in enumerate(not_pi_user_dai_list):
-                new_dict = {'ban_yuan': {"id": user_min_counter[index].id, "name": user_min_counter[index].name},
-                            'dai_ban': {"id": item.id, "name": item.name}}
-                wan_ban_list.append(new_dict)
-        else:
-
-            for index, item in enumerate(not_pi_user_list):
-                new_dict = {'ban_yuan': {"id": item.id, "name": item.name},
-                            'dai_ban': {"id": user_dai_min_counter[index].id, "name": user_dai_min_counter[index].name}}
-                wan_ban_list.append(new_dict)
-
-        # 合一，随机处理
-        if not bai_ban_list or not wan_ban_list or len(Users_bao_an.objects.all()) > 3:
-            msg = "带班人员或值班人员为空(包括值班保安)！"
-            return render(request, 'index/index.html', locals())
-        bai_ban_list = list(np.random.permutation(bai_ban_list))
-        wan_ban_list = list(np.random.permutation(wan_ban_list))
-        a = 1
-        for i in range(delta + 1):
-            new = {}
-
-            if len(bai_ban_list) <= i + 1:
-                bai_ban_list += bai_ban_list
-            if len(wan_ban_list) <= i + 1:
-                wan_ban_list += wan_ban_list
-
-
-            new_delta = datetime.timedelta(days=1)
-
-            new['date'] = (date_start + new_delta * i).strftime("%m月%d日")
-            new['bai_ban'] = bai_ban_list[i]
-            new['wan_ban'] = wan_ban_list[i]
-
-            list_he.append(new)
-        data = list_he
+    elif delta >= 32:
+        msg = "日期距离不能大于31天！"
         return render(request, 'index/index.html', locals())
+
+    relation = {
+
+    }
+    # 获取时间范围
+    date_range = [date_start + datetime.timedelta(days=x) for x in range((date_end - date_start).days + 1)]
+
+    for i in date_range:
+        relation[i.strftime('%Y%m%d')] = get_name(i)
+    context = {
+        'method': 'POST',
+        'data': relation,
+        'week': date_range
+    }
+    for k, v in relation.items():
+        print(k)
+        for i in v:
+            print(f"{i}: hxkc")
+        print(".....")
+    print(relation)
+    return render(request, 'index/index.html', context)
 
 
 def download(request):
