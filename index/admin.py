c7a3f560889196394fb20717a1ac6097b2060f67
CaptainRong <cr1092014130@gmail.com>
2024-01-01 15:44:06 +0800

    增加选定时间后显示该时间段内值班人员的内容，remove something useless

--- index/admin.py
+++ index/admin.py
@@ -14,14 +14,14 @@ admin.site.index_title = "值班管理"
 # django-import-export
 class UsersResource(resources.ModelResource):
     class Meta:
-        model = Users
+        model = MyUsers
 
 
 class UsersAdmin(ImportExportModelAdmin):
-    list_display = ['id', 'name', 'department', 'counter', 'gender', 'phone', 'is_active']
-    list_display_links = ['id', 'name']
+    list_display = ('id', 'user', 'department', 'gender', 'is_active')
+    list_display_links = ['id', 'user']
     resource_classes = [UsersResource]
-    search_fields = ('name', 'phone')
+    search_fields = ('user', 'department')
     ordering = ['id']
     list_filter = ['gender']
     # list_per_page = 20
@@ -32,24 +32,23 @@ admin.site.register(Users, UsersAdmin)
 
 """     ==========  带班 ==============  """
 
-
-class Users_daiResource(resources.ModelResource):
-    class Meta:
-        model = Users_dai_ban
-
-
-class Users_daiAdmin(ImportExportModelAdmin):
-    list_display = ['id', 'name', 'department', 'counter', 'gender', 'phone', 'is_active']
-    list_display_links = ['id', 'name']
-    resource_classes = [Users_daiResource]
-    search_fields = ('name', 'phone')
-    ordering = ['id']
-    list_filter = ['gender']
-    # list_per_page = 20
-    list_editable = ['gender', 'department', 'is_active']
-
-
-admin.site.register(Users_dai_ban, Users_daiAdmin)
+# class Users_daiResource(resources.ModelResource):
+#     class Meta:
+#         model = Users_dai_ban
+#
+#
+# class Users_daiAdmin(ImportExportModelAdmin):
+#     list_display = ['id', 'name', 'department', 'counter', 'gender', 'phone', 'is_active']
+#     list_display_links = ['id', 'name']
+#     resource_classes = [Users_daiResource]
+#     search_fields = ('name', 'phone')
+#     ordering = ['id']
+#     list_filter = ['gender']
+#     # list_per_page = 20
+#     list_editable = ['gender', 'department', 'is_active']
+#
+#
+# admin.site.register(Users_dai_ban, Users_daiAdmin)
 
 """     ==========  科室 ==============  """
 
@@ -74,55 +73,52 @@ admin.site.register(Department, DepartmentsAdmin)
 
 """     ==========  值班匹配 ==============  """
 
-
-class MatchingResource(resources.ModelResource):
-    class Meta:
-        model = Matching
-
-
-class MatchingAdmin(ImportExportModelAdmin):
-    list_display = ['id', 'user', 'user_dai', 'counter', "type"]
-    list_display_links = ['id']
-    resource_classes = [MatchingResource]
-    search_fields = ('user', 'user_dai')
-    # ordering = ['name']
-    # list_filter = ['user', 'user_id']
-    # list_per_page = 20
-    list_editable = ['user', 'user_dai', 'type']
-
-
-admin.site.register(Matching, MatchingAdmin)
+# class MatchingResource(resources.ModelResource):
+#     class Meta:
+#         model = Matching
+#
+#
+# class MatchingAdmin(ImportExportModelAdmin):
+#     list_display = ['id', 'user', 'user_dai', 'counter', "type"]
+#     list_display_links = ['id']
+#     resource_classes = [MatchingResource]
+#     search_fields = ('user', 'user_dai')
+#     # ordering = ['name']
+#     # list_filter = ['user', 'user_id']
+#     # list_per_page = 20
+#     list_editable = ['user', 'user_dai', 'type']
+#
+#
+# admin.site.register(Matching, MatchingAdmin)
 
 """     ==========  保安 ==============  """
 
-
-class Users_bao_anResource(resources.ModelResource):
-    class Meta:
-        model = Users_bao_an
-
-
-class Users_bao_anAdmin(ImportExportModelAdmin):
-    list_display = ['id', 'name', 'counter', 'gender', 'phone', 'is_active']
-    list_display_links = ['id']
-    resource_classes = [Users_bao_anResource]
-    search_fields = ('name', 'phone')
-    # ordering = ['name']
-    # list_filter = ['user', 'user_id']
-    # list_per_page = 20
-    list_editable = ['is_active']
+# class Users_bao_anResource(resources.ModelResource):
+#     class Meta:
+#         model = Users_bao_an
 
 
-admin.site.register(Users_bao_an, Users_bao_anAdmin)
+# class Users_bao_anAdmin(ImportExportModelAdmin):
+#     list_display = ['id', 'name', 'counter', 'gender', 'phone', 'is_active']
+#     list_display_links = ['id']
+#     resource_classes = [Users_bao_anResource]
+#     search_fields = ('name', 'phone')
+#     # ordering = ['name']
+#     # list_filter = ['user', 'user_id']
+#     # list_per_page = 20
+#     list_editable = ['is_active']
+#
+#
+# admin.site.register(Users_bao_an, Users_bao_anAdmin)
 """     ==========  日志 ==============  """
-
 
 # class LogResource(resources.ModelResource):
 #     class Meta:
-#         model = Log
+#         model = MyUsers
 #
 #
 # class LogAdmin(ImportExportModelAdmin):
-#     list_display = ['id', 'numbers_s', 'numbers_e', 'time', 'is_active']
+#     list_display = ['id', 'user', 'department', 'is_active']
 #     list_display_links = ['id']
 #     resource_classes = [LogResource]
 #     # search_fields = ('name', 'phone')
